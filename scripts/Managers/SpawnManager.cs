279c076f48b7f3d4cc0711dc3961719a3215394c
Kristofer Koehn <fernandoprofitt@gmail.com>
2024-04-06 07:25:22 -0700

    test

--- scripts/Managers/SpawnManager.cs
+++ scripts/Managers/SpawnManager.cs
@@ -197,11 +197,11 @@ namespace MMOTest.scripts.Managers
             StatManager.GetInstance().AssignStatBlock(sb.SerializeStatBlock(), ActorID);
 
             Vector3 spawnPosition = GetValidSpawnPosition((Teams)statsDict[StatType.CTF_TEAM]);
-            em.GlobalPosition = spawnPosition;
 
             GD.Print(spawnPosition);
 
             SceneOrganizerManager.GetInstance().CurrentLevel.GetNode<Node>("PuppetModels").AddChild(em,forceReadableName:true);
+            em.GlobalPosition = spawnPosition;
 
         }
 
