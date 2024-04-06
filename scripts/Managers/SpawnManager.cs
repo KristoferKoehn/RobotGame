3f630030069bce3aea50324aa5493ab780741a64
Kristofer Koehn <fernandoprofitt@gmail.com>
2024-04-06 07:28:05 -0700

    enemy controller test

--- scripts/Managers/SpawnManager.cs
+++ scripts/Managers/SpawnManager.cs
@@ -192,12 +192,16 @@ namespace MMOTest.scripts.Managers
             a.PuppetModelReference = em;
 
 
+            EnemyController enemyController = new EnemyController();
+            SceneOrganizerManager.GetInstance().CurrentLevel.AddChild(enemyController);
+
             ActorManager.GetInstance().actors[ActorID] = a;
 
             StatManager.GetInstance().AssignStatBlock(sb.SerializeStatBlock(), ActorID);
 
             Vector3 spawnPosition = GetValidSpawnPosition((Teams)statsDict[StatType.CTF_TEAM]);
-
+            enemyController.GlobalPosition = spawnPosition;
+            enemyController.AttachModel(em);
             GD.Print(spawnPosition);
 
             SceneOrganizerManager.GetInstance().CurrentLevel.GetNode<Node>("PuppetModels").AddChild(em,forceReadableName:true);
